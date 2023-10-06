7f405083df81058387debbe443609710a08fe359
Aykhan-229 <teymurlu@ualberta.ca>
2023-10-05 18:34:13 -0600

    added color string

--- app/src/main/java/com/example/myapplication/Diamond.java
+++ app/src/main/java/com/example/myapplication/Diamond.java
@@ -1,9 +1,10 @@
 package com.example.myapplication;
 
 public class Diamond extends Shape {
+    private String color = "Green";
     public Diamond(int x, int y) {
         super(x, y);
     }
-    
+
 
 }
