fa9fcf258b7aef1a256c2be5af60ce986352c0e6
ethanman102 <107374579+ethanman102@users.noreply.github.com>
2023-10-05 18:54:51 -0600

    Update Shape.java

--- app/src/main/java/com/example/myapplication/Shape.java
+++ app/src/main/java/com/example/myapplication/Shape.java
@@ -4,7 +4,7 @@ public abstract class Shape {
     private int x;
     private int y;
 
-    private String color ="Green";
+    private String color ="black";
 
     Shape(int x, int y){
         this.x = x;
