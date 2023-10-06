fd3d85d91f86e83b22d9718f89718f87e015db90
Aykhan-229 <teymurlu@ualberta.ca>
2023-10-05 19:15:38 -0600

    coor

--- app/src/main/java/com/example/myapplication/Shape.java
+++ app/src/main/java/com/example/myapplication/Shape.java
@@ -4,7 +4,7 @@ public abstract class Shape {
     private int x;
     private int y;
 
-    private String color = "lokj";
+    private String color = "yellow";
 
     Shape(int x, int y){
         this.x = x;
